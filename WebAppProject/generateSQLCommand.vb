13ac35d51ed581156922c529c75a87e114dc0a50
JaelAguilar <cjaelaguilar@gmail.com>
2023-06-03 22:44:35 -0700

    Tables A.1.1, A.1.2, A.1.3

--- WebAppProject/generateSQLCommand.vb
+++ WebAppProject/generateSQLCommand.vb
@@ -1,4 +1,5 @@
 ﻿Imports System.Data.SqlClient
+Imports System.Security.Cryptography
 
 Partial Class WebForm1
     Private Function GenerateSQLCommand(sql As SqlCommand, r As DataRow)
@@ -34,6 +35,71 @@ Partial Class WebForm1
                     .Parameters.AddWithValue("@elab", r(19))
                     .Parameters.AddWithValue("@rev", r(20))
                     .Parameters.AddWithValue("@aut", r(21))
+                End With
+
+            Case "A.1.1"
+                initialQuery &= "INSERT INTO A.1.1
+(Secretaria, Dirección,Impuestos,Cuot_ApSS,Cont_Mej,Derechos,Productos,Aprov,Ing_Vta_Bs,Part_Apor,Tras_Sub_Oayu,Ing_Finan,ClaveEjercicio,Elaboró,Revisó,Autorizó)"
+                initialQuery &= "VALUES
+(@Direccion,@Impuestos,@Cuot_ApSS,@Cont_Mej,@Derechos,@Productos,@Aprov,@Ing_Vta_Bs,@Part_Apor,@Tras_Sub_Oayu,@Ing_Finan,@ClaveEjercicio,@Elaboró,@Revisó,@Autorizó)"
+                With sql
+                    .CommandText = initialQuery
+                    .Parameters.AddWithValue("@Direccion", r(0))
+                    .Parameters.AddWithValue("@Impuestos", r(1))
+                    .Parameters.AddWithValue("@Cuot_ApSS", r(2))
+                    .Parameters.AddWithValue("@Cont_Mej", r(3))
+                    .Parameters.AddWithValue("@Derechos", r(4))
+                    .Parameters.AddWithValue("@Producto", r(5))
+                    .Parameters.AddWithValue("@Aprov", r(6))
+                    .Parameters.AddWithValue("@Ing_Vta_Bs", r(7))
+                    .Parameters.AddWithValue("@Part_Apor", r(8))
+                    .Parameters.AddWithValue("@Tras_Sub_Oayu", r(9))
+                    .Parameters.AddWithValue("@Ing_Finan", r(10))
+                    .Parameters.AddWithValue("@ClaveEjercicio", r(11))
+                    .Parameters.AddWithValue("@Elaboró", r(12))
+                    .Parameters.AddWithValue("@Revisó", r(13))
+                    .Parameters.AddWithValue("@Autorizó", r(14))
+                End With
+
+            Case "A.1.2"
+                initialQuery &= "INSERT INTO A.1.2
+(Secretaria, Dirección,Serv_Per,Mat_Sum,Serv_Gen,Tras_Sub_Oayu,Bien_Mu_Inm_Inta,Iver_Pub,Inver_Fin_OP,Part_Apor,Dedua_Pub,FechaCorte,Elaboró,Revisó,Autorizó)"
+                initialQuery &= "VALUES
+(@Secretaria,@Dirección,@Serv_Per,@Mat_Sum,@Serv_Gen,@Tras_Sub_Oayu,@Bien_Mu_Inm_Inta,@Iver_Pub,Inver_Fin_OP,@Part_Apor,@Dedua_Pub,@FechaCorte,@Elaboró,@Revisó,@Autorizó)"
+                With sql
+                    .CommandText = initialQuery
+                    .Parameters.AddWithValue("@Secretaria", r(0))
+                    .Parameters.AddWithValue("@Dirección", r(1))
+                    .Parameters.AddWithValue("@Serv_Per", r(2))
+                    .Parameters.AddWithValue("@Mat_Sum", r(3))
+                    .Parameters.AddWithValue("@Serv_Gen", r(4))
+                    .Parameters.AddWithValue("@Tras_Sub_Oayu", r(5))
+                    .Parameters.AddWithValue("@Bien_Mu_Inm_Inta", r(6))
+                    .Parameters.AddWithValue("@Iver_Pub,Inver_Fin_OP", r(7))
+                    .Parameters.AddWithValue("@Part_Apor", r(8))
+                    .Parameters.AddWithValue("@Dedua_Pub", r(9))
+                    .Parameters.AddWithValue("@FechaCorte", r(10))
+                    .Parameters.AddWithValue("@Elaboró", r(11))
+                    .Parameters.AddWithValue("@Revisó", r(12))
+                    .Parameters.AddWithValue("@Autorizó", r(13))
+                End With
+
+            Case "A.1.3"
+                initialQuery &= "INSERT INTO A.1.3
+(Secretaria,Dirección,Clave,Nombre,Presup_Auto,Porcentaje,FechaCorte,Elaboró,Revisó)"
+                initialQuery &= "VALUES
+(@Secretaria,@Dirección,@Clave,@Nombre,@Presup_Auto,@Porcentaje,@FechaCorte,@Elaboró,@Revisó,)"
+                With sql
+                    .CommandText = initialQuery
+                    .Parameters.AddWithValue("@ecretaria", r(0))
+                    .Parameters.AddWithValue("@Dirección", r(1))
+                    .Parameters.AddWithValue("@Clave", r(2))
+                    .Parameters.AddWithValue("@Nombre", r(3))
+                    .Parameters.AddWithValue("@Presup_Auto", r(4))
+                    .Parameters.AddWithValue("@Porcentaje", r(5))
+                    .Parameters.AddWithValue("@FechaCorte", r(6))
+                    .Parameters.AddWithValue("@Elaboró", r(7))
+                    .Parameters.AddWithValue("@Revisó", r(8))
                 End With
         End Select
     End Function
